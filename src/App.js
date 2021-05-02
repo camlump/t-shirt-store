5105872fb79030fbfb490eddfd45c3828919671f
CameronLumpkinATL <camlump@yahoo.com>
2021-05-02 18:29:08 -0400

    finished cart component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {commerce } from './lib/commerce'
 // import Products from './components/Products'
 // import Navbar from './components/Navbar/Navbar'
 
-import {Products, Navbar } from './components'
+import {Products, Navbar, Cart } from './components'
 
 const App = () => {
 
@@ -38,7 +38,8 @@ const App = () => {
     return (
         <div>
             <Navbar totalItems={cart.total_items} />
-            <Products products={products} onAddToCart={handleAddToCart} />
+            {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+            <Cart cart={cart} />
         </div>
     )
 }
